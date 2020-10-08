22a9102917843b5cbc4a4ec9ab92e26af444c76d
JaxonTeller <nishuzephyrus@outlook.com>
2020-10-08 14:01:01 +0530

    Using @NoRepositoryBeanDefination for restricting Spring from creating beans

--- src/main/java/com/intorduction/springdatarepositories/Repository/CandidateRepository.java
+++ src/main/java/com/intorduction/springdatarepositories/Repository/CandidateRepository.java
@@ -2,8 +2,9 @@ package com.intorduction.springdatarepositories.Repository;
 
 import com.intorduction.springdatarepositories.Resource.Candidate;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
-
+@NoRepositoryBean
 /*Generic Repository- not tied to any specific persistence technology*/
 public interface CandidateRepository extends CrudRepository<Candidate,Long> {
 }
--- src/main/java/com/intorduction/springdatarepositories/Repository/CandidateRepositoryUsingRepoDefn.java
+++ src/main/java/com/intorduction/springdatarepositories/Repository/CandidateRepositoryUsingRepoDefn.java
@@ -4,6 +4,7 @@ import com.intorduction.springdatarepositories.Resource.Candidate;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.RepositoryDefinition;
 
+@NoRepositoryBean
 @RepositoryDefinition(domainClass = Candidate.class,idClass = Long.class)
 public interface CandidateRepositoryUsingRepoDefn {
 }
